0afb08fb3d64f99ba21034d315a0c846e5c7159c
RudraDeshmukh <99274112+RudraDeshmukh@users.noreply.github.com>
2022-03-15 20:57:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-//https://teachablemachine.withgoogle.com/models/qwZ00kiks/
+function audio_classifier(){
+    navigator.mediaDevices.getUserMedia({audio : true})
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/qwZ00kiks/model.json',model_loaded)
+}
+function model_loaded(){
+    classifier.classify(gotResults())
+}
